e7413ae716c8a5c928a6ef4da8e992537eaa63e9
Koniukhova Sofia <sovunja1712@gmail.com>
2024-03-27 17:38:13 +0300

    Watched lessons 11 - 19

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -1,5 +1,13 @@
+// Core modules
 const fs = require('fs');
+const http = require('http');
+const url = require('url');
 
+// Our own modules
+const replaceTemplate = require('./modules/replaceTemplate');
+
+////////////////////////////////////////////////
+// FILES
 //Blocking, synchronous way
 /* const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
 console.log(textIn);
@@ -8,7 +16,7 @@ fs.writeFileSync('./txt/output.txt', textOut);
 console.log('File written!'); */
 
 //Non-blocking, asynchronous way
-fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
+/* fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
    if (err) return console.log('ERROR!');
 
    fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
@@ -22,4 +30,55 @@ fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
       })
    })
 })
-console.log('Will read file!');
+console.log('Will read file!'); */
+
+
+////////////////////////////////////////////////
+// SERVER
+
+const templateOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8');
+const templateCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8');
+const templateProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`, 'utf-8');
+
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
+const dataObj = JSON.parse(data);
+
+const server = http.createServer((req, res) => {
+   const { query, pathname } = url.parse(req.url, true);
+
+   // Overview page
+   if (pathname === '/' || pathname === '/overview') {
+      res.writeHead(200, { 'Content-type': 'text/html' });
+
+      const cardsHtml = dataObj.map(el => replaceTemplate(templateCard, el)).join('');
+      const output = templateOverview.replace('{%PRODUCT_CARDS%}', cardsHtml);
+
+      res.end(output);
+
+   // Product page
+   } else if (pathname === '/product') {
+      res.writeHead(200, { 'Content-type': 'text/html' });
+      const product = dataObj[query.id];
+      const output = replaceTemplate(templateProduct, product);
+
+      res.end(output);
+
+   // API
+   } else if (pathname === '/api') {
+      res.writeHead(200, { 'Content-type': 'application/json' });
+      res.end(data);
+
+   // Not found
+   } else {
+      res.writeHead(404, {
+         'Content-type': 'text/html',
+         'my-own-header': 'hello-world'
+      });
+      res.end('<h1>Page not found</h1>');
+   }
+
+});
+
+server.listen(8000, '127.0.0.1', () => {
+   console.log('Listening to requests on port 8000');
+});
